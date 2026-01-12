78f79ca387d20f38d48e1f3bbfd22b74b5ecc0d5
vincetheprogrammer <vincetheprogrammer@gmail.com>
2026-01-11 20:47:57 -0600

    setup github pages

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -4,6 +4,8 @@ import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
+  // needed for production when deployed on github pages
+  base: '/lemonline/',
   // This block only matters when running in dev mode.
   server: {
     port: 3000,
